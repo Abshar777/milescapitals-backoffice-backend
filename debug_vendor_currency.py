01641837c38ed102a3b873ca5d7c03d5f9d87228
Abshar777 <absharameen625@gmail.com>
2026-03-19 19:21:36 +0530

    done

--- debug_vendor_currency.py
+++ debug_vendor_currency.py
@@ -23,8 +23,8 @@ import json
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGODB_URI") or os.getenv("MONGO_URI") or "mongodb://localhost:27017"
-DB_NAME   = os.getenv("DB_NAME", "miles_ac")
+MONGO_URI = "mongodb://delta:123@31.97.237.248:27017"
+DB_NAME   = "miles_ac"
 
 def fmt(val):
     """Pretty-print a document, hiding _id ObjectId noise."""
